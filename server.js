1432e4a55de78b0e3eb1ce059a63851ec325d090
unknown <daniel@danielstern.ca>
2014-10-28 07:52:40 -0400

    more-btr

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end("HI!");
+	res.end("Hello world?");
 })
 
 var port = Number(process.env.PORT || 3000);
