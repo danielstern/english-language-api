611f7401b579eb966640cdc5b1b67d89a7b8ca8b
unknown <daniel@danielstern.ca>
2014-10-28 08:17:32 -0400

    parsing-shit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http'),
-fs = require('fs'),
+dictionary = require('./data/dictionary.json'),
+// fs = require('fs'),
 sys = require("sys"),
 url = require("url");
 
@@ -8,46 +9,25 @@ var port = Number(process.env.PORT || 3000);
 http.createServer(function(request,response){
 	sys.puts("I got kicked");
 	   //var _get = url.parse(request.url, true).query;
-   fs.readFile('./data/dict.txt', 'utf-8', function (error, data) {
+   // fs.readFile('./data/dictionary.json.txt', 'utf-8', function (error, data) {
       // Write headers.
       response.writeHead(200, {'Content-Type': 'text/plain'});
       // Increment the number obtained from file.
       // data = parseInt(data) + 1;
+      // var words
+      // resoi
       // Write incremented number to file.
       // fs.writeFile('test.txt', data);
       // End response with some nice message.
-      response.end(data);
+      for (var k in dictionary) {
+      	response.write(k);	
+      	response.write('\n');
+      }
+      // var words = JSON.parse(data);
+      
+      response.end("OK?");
+      // response.end(dictionary);
       // response.end('This page was refreshed ' + data + ' times!');
-   });
+   // });
 
 }).listen(port);
-
-// fs.readFile('./data/dict.txt', function (err, html) {
-//     if (err) {
-//         throw err; 
-//     }       
-//     // http.createServer(function(request, response) {  
-//     //     response.writeHeader(200, {"Content-Type": "text/html"});  
-//     //     response.write(html);  
-//     //     response.end();  
-//     // }).listen(8000);
-
-
-// 		var server = http.createServer(function(req,res){
-// 			res.writeHead(200, {'Content-Type':'text/html'});
-// 			res.write(html);
-// 			// res.end("Hello world?");
-
-
-
-// 			server.listen(port);
-// 		})
-// });
-
-
-// var server2 = http.createServer(function(req,res){
-// 	res.writeHead(200, {'Content-Type':'text/html'});
-// 	res.end("Hello world?");
-// })
-
-// server2.listen(666);
